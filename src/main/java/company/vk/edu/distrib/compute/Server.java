31d40ad9ce65a3716f3d38b125ff13d10ccfdeba
Igor Popov <ip-adsense@yandex.ru>
2026-04-06 08:14:00 +0000

    merge from upstream, rename my KVServiceFactoryImpl to uniq, update tests

--- src/main/java/company/vk/edu/distrib/compute/Server.java
+++ src/main/java/company/vk/edu/distrib/compute/Server.java
@@ -2,7 +2,7 @@ package company.vk.edu.distrib.compute;
 
 import module java.base;
 
-import company.vk.edu.distrib.compute.ip.KVServiceFactoryImpl;
+import company.vk.edu.distrib.compute.ip.PopovIgorKVServiceFactoryImpl;
 
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +11,7 @@ public class Server {
     void main() throws IOException {
         var log = LoggerFactory.getLogger("server");
         var port = 8080;
-        KVService storage = new KVServiceFactoryImpl().create(port);
+        KVService storage = new PopovIgorKVServiceFactoryImpl().create(port);
         storage.start();
         log.info("Server started on port {}", port);
         Runtime.getRuntime().addShutdownHook(new Thread(storage::stop));
